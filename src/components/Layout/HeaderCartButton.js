cc7e22cb8be2bc2ec0a588b5e70c5959e85689b2
Luc Ho <hotriluc@Lucs-MacBook-Pro.local>
2022-04-25 14:59:02 +0200

    cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,17 @@
 import React from "react";
+import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
-  console.log(classes);
-  return <button className={classes["header__cart-btn"]}>Your cart</button>;
+  return (
+    <button className={classes["header__cart-btn"]}>
+      <span className={classes["cart-icon"]}>
+        <CartIcon />
+      </span>
+      <span>Your cart</span>
+      <span className={classes["badge"]}>3</span>
+    </button>
+  );
 };
 
 export default HeaderCartButton;
