b60ee9e9fb34ea6ffb03de6d5b4088a3fde2136e
Luc Ho <hotriluc@Lucs-MacBook-Pro.local>
2022-04-26 16:10:18 +0200

    meal item style

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -1,9 +1,11 @@
+import classes from "./MealItem.module.css";
+
 const MealItem = (props) => {
   return (
-    <li key={props.id}>
-      <h3>{props.name}</h3>
-      <p>{props.description}</p>
-      <p>{props.price}</p>
+    <li key={props.id} className={classes.meal}>
+      <h3 className={classes["meal__title"]}>{props.name}</h3>
+      <p className={classes["meal__description"]}>{props.description}</p>
+      <p className={classes["meal__price"]}>${props.price}</p>
     </li>
   );
 };
