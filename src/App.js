52a8e50c76c2ed287c69cb270718e7162b8440fc
Luc Ho <hotriluc@Lucs-MacBook-Pro.local>
2022-04-26 14:15:47 +0200

    meal component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header logo="React meals" />
-      <p>meals</p>
+      <Meals />
     </>
   );
 }
